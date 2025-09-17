c57e35441f91fb17b1399e0e068efb037e377128
asgofc <email@example.com>
2025-09-17 18:37:43 +0300

    structure in todo app

--- golang_projects/todoApp/main.go
+++ golang_projects/todoApp/main.go
@@ -1,61 +1,32 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
+	"time"
+
+	"github.com/google/uuid"
 )
 
-// Тип для списка задач
-type Todos []string
-
-// Универсальное хранилище (generic)
-type Storage[T any] struct {
-	filename string
+type Task struct {
+	ID           uuid.UUID `json:"id"`
+	CreationDate time.Time `json:"creation_date"`
+	Task         string    `json:"task"`
+	State        bool      `json:"state"`
 }
 
-func NewStorage[T any](filename string) *Storage[T] {
-	return &Storage[T]{filename: filename}
-}
-
-// Загрузка из файла
-func (s *Storage[T]) Load(data *T) error {
-	file, err := os.Open(s.filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	return json.NewDecoder(file).Decode(data)
-}
-
-// Сохранение в файл
-func (s *Storage[T]) Save(data *T) error {
-	file, err := os.Create(s.filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	return json.NewEncoder(file).Encode(data)
-}
+const JSON_PATH = "todo.json"
 
 func main() {
-	// Инициализируем пустой список задач
-	todos := Todos{}
+	tasks := []Task{}
 
-	// Создаем сторедж, привязанный к файлу todos.json
-	storage := NewStorage[Todos]("todos.json")
-
-	// Пробуем загрузить задачи
-	err := storage.Load(&todos)
-	if err != nil {
-		fmt.Println("Warning: Could not load todos from storage. Starting fresh todos.")
+	t := Task{
+		ID:           uuid.New(), // генерируем новый уникальный ID
+		CreationDate: time.Now(),
+		Task:         "Сделать проект",
+		State:        false,
 	}
 
-	// Добавим тестовую задачу и сохраним
-	todos = append(todos, "Выучить Go")
-	_ = storage.Save(&todos)
+	tasks = append(tasks, t)
+	fmt.Print(tasks)
 
-	fmt.Println("Todos:", todos)
 }
